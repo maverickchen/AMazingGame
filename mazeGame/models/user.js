5e9d78c6345f550a0a02e27e9f160d60bafa3ee8
maverickchen <mcmaverick001@gmail.com>
2018-04-02 00:44:12 -0400

    add methods to update number of wins, username, and email of an account in database

--- mazeGame/models/user.js
+++ mazeGame/models/user.js
@@ -2,8 +2,8 @@
 var db = require('../db.js')
 
 exports.create = function(username, password, email, done) {
-    var values = [username, password, email]
-    db.get().query('INSERT INTO users (username, password, email) VALUES(?, ?, ?)', values, function(err,result) {
+    var values = [username, password, email, 0]
+    db.get().query('INSERT INTO users (username, password, email, numWins) VALUES(?, ?, ?, ?)', values, function(err,result) {
         if (err) return done(err)
         done(null, result.insertId)
     })
@@ -15,3 +15,21 @@ exports.get = function(username, done) {
     done(null, rows)
   })
 }
+
+exports.updateEmail = function(username, newEmail, done) {
+    db.get().query('UPDATE users SET email = ?,  WHERE username = ?', newEmail, username, function (err,rows) {
+        if (err) return done(err);
+    });
+};
+
+exports.updateUsername = function(oldUsername, newUsername,done) {
+    db.get().query('UPDATE users SET username = ?,  WHERE username = ?', newUsername, oldUsername, function (err,rows) {
+        if (err) return done(err);
+    });
+};
+
+exports.updateNumWins = function(username, numWins, newUsername,done) {
+    db.get().query('UPDATE users SET numWins = ?,  WHERE username = ?', numWins, username, function (err,rows) {
+        if (err) return done(err);
+    });
+};
