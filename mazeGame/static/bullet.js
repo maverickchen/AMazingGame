e6594954d5c6053b2555e4e5097dd2ff14fd29aa
JiangtianQian <kakaxiidea@gmail.com>
2018-04-27 23:25:43 -0400

    add some commit to the bullet.js

--- mazeGame/static/bullet.js
+++ mazeGame/static/bullet.js
@@ -1,5 +1,6 @@
 // A class to represent a bullet
 "use strict";
+// The speed of the bullets 
 var speed = 1200;
 module.exports = class Bullet {
     constructor(x, y, orientation, teamID) {
@@ -7,35 +8,26 @@ module.exports = class Bullet {
         this.y = y;
         this.width = 20;
         this.height = 20;
+        // Initialize the direction of bullets
+  
         this.y_dir = 0;
         this.x_dir = 0;
-        // Add x direction and y direction to the bullet
+
         if (orientation == 'd') this.y_dir = 1;
         else if (orientation == 'u') this.y_dir = -1;
         else if (orientation == 'l') this.x_dir = -1;
         else if (orientation == 'r') this.x_dir = 1;
 
-        /*this.x_dir = x_dir;
-        this.y_dir = y_dir;*/
         
-        //console.log(this.x_dir);
-        //console.log(this.y_dir);
-
         this.owner = teamID; // id of the player that shot this bullet
     }
 
+    
     use(player) {
         player.health -= 10;
         if (player.health < 0) player.health = 0;
     }
-
-    // returns false if 
     move(deltaT, maze) {
-
-        //console.log("x_dir in the bullet " + this.x_dir);
-        //console.log("x in the bullet " + this.x);
-
-
         var x1 = Math.floor((this.x + speed*deltaT*this.x_dir) / 100);
         var x2 = Math.floor((this.x + this.width + speed*deltaT*this.x_dir) / 100);
         var y1 = Math.floor((this.y + speed*deltaT*this.y_dir ) / 100);
@@ -47,9 +39,6 @@ module.exports = class Bullet {
         if (y2 < 0 || y2 >= maze.length) return;
 
         // If there is a path, the bullet could go through
-        //console.log("x1 in bullet " + x1);
-        //console.log("y1 in bullet " + y1);
-
         if (maze[y1][x1] == 1 && maze[y2][x1] == 1 && maze[y1][x2] == 1 && maze[y2][x2] == 1) {
             this.x = this.x + speed*deltaT*this.x_dir;
             this.y = this.y + speed*deltaT*this.y_dir;
